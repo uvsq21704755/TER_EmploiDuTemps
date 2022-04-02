7a94bd5eddce966dd3f5a0926de9fbea7d26c28d
Julie Varenne-Paquet <72136120+julieVP@users.noreply.github.com>
2022-04-02 11:27:41 +0200

    Create module.sql
    Ajout des modules (juste M1 Informatique)

---
+++ SQL/module.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Module
+(
+    Code VARCHAR(10) PRIMARY KEY NOT NULL,
+    Intitule VARCHAR(100)
+)
+
+INSERT INTO module VALUES 
+('MIN17214','Conception de BD'),
+('MIN17211','Methodes de Ranking'),
+('MIN17217','Application Web et Securite'),
+('MIN17213','Tuning de BD'),
+('MIN17215','Protocoles IP'),
+('MIN17218','Calcul Securise'),
+('MIN17212','Simulation'),
+('MIN17216','Reseaux etendus');
