f58da593bfa36214a597d3e6ebdc5e9335a97a63
Julie Varenne-Paquet <72136120+julieVP@users.noreply.github.com>
2022-04-13 20:59:39 +0200

    Update seance.sql
    Mise en ordre de la table;
    Ajout des cours pour Tuning + Conception + Ranking + AWS
    Va jusqu'à 'Deschamps', 'Chamila' (comprise)

--- SQL/seance.sql
+++ SQL/seance.sql
@@ -4,22 +4,21 @@ CREATE table Seance
 	Jour Varchar(50),
 	HeureDebut Varchar(5),
 	HeureFin Varchar(5),
-  IdEnseignant int, 
-  NomSalle varchar(50)
+	IdEnseignant int,
+	NomSalle varchar(50)
+	FOREIGN KEY(IdEnseignant) REFERENCES Enseignant(num_ens)
+	FOREIGN KEY(NomSalle) REFERENCES Salle(NomSalle)
 );
 
-INSERT INTO Seance VALUES(1, 'Lundi', '9h40', '12h50', 90162770, 'Amphi E');
-INSERT INTO Seance VALUES(2, 'Mardi', '9h40', '12h50', 85045461, 'Amphi E');
-INSERT INTO Seance VALUES(3, 'Mercredi', '9h40', '12h50', 85028604, 'Amphi E');
-INSERT INTO Seance VALUES(4, 'Jeudi', '9h40', '12h50', 24752580, 'Amphi E');
-INSERT INTO Seance VALUES(5, 'Vendredi', '9h40', '12h50', 73710800, 'Amphi E');
-INSERT INTO Seance VALUES(6, 'Lundi', '13h50', '15h20', 85553092, 'Amphi E');
-INSERT INTO Seance VALUES(7, 'Mardi', '13h50', '18h40', 85045461, 'G105');
-INSERT INTO Seance VALUES(8, 'Mercredi', '13h50', '18h40', 85045461, 'G105');
-INSERT INTO Seance VALUES(9, 'Mercredi', '13h50', '18h40', 85028604, 'JUNGLE');
-INSERT INTO Seance VALUES(10, 'Jeudi', '9h40', '12h50', 81951043, 'Amphi H');
-INSERT INTO Seance VALUES(11, 'Jeudi', '13h50', '18h40', 73710800, 'JUNGLE');
-INSERT INTO Seance VALUES(11, 'Jeudi', '13h50', '18h40', 39047430, 'G203');
-INSERT INTO Seance VALUES(11, 'Jeudi', '13h50', '18h40', 81951043, 'G204');
+INSERT INTO Seance VALUES(1, 'Mardi', '9h40', '12h50', 85045461, 'Amphi E');
+INSERT INTO Seance VALUES(2, 'Mercredi', '9h40', '12h50', 85028604, 'Amphi E');
+INSERT INTO Seance VALUES(3, 'Vendredi', '9h40', '12h50', 73710800, 'Amphi E');
+INSERT INTO Seance VALUES(4, 'Mardi', '13h50', '18h40', 85045461, 'G105');
+INSERT INTO Seance VALUES(5, 'Mercredi', '13h50', '18h40', 85045461, 'G105');
+INSERT INTO Seance VALUES(6, 'Mercredi', '13h50', '18h40', 85028604, 'JUNGLE');
+INSERT INTO Seance VALUES(7, 'Jeudi', '9h40', '12h50', 81951043, 'Amphi H');
+INSERT INTO Seance VALUES(8, 'Jeudi', '13h50', '18h40', 73710800, 'JUNGLE');
+INSERT INTO Seance VALUES(9, 'Jeudi', '13h50', '18h40', 81951043, 'G203');
+INSERT INTO Seance VALUES(10, 'Jeudi', '13h50', '18h40', 39047430, 'G204');
 INSERT INTO Seance VALUES(11, 'Vendredi', '13h50', '18h40', 85028604, 'ALSACE');
-INSERT INTO Seance VALUES(11, 'Vendredi', '13h50', '18h40', 73710800, 'RC22');
+INSERT INTO Seance VALUES(12, 'Vendredi', '13h50', '18h40', 73710800, 'RC22');
