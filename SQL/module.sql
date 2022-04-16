f0702d48b871a32e4722d8b5cf2d5e9f6e2c493d
Julie Varenne-Paquet <72136120+julieVP@users.noreply.github.com>
2022-04-16 13:00:47 +0200

    Update module.sql
    Ajout de groupe + MAJ des insert

--- SQL/module.sql
+++ SQL/module.sql
@@ -1,14 +1,19 @@
+-- Création de la table Enseignant --
+
 CREATE TABLE Module
 (
     Code VARCHAR(10) PRIMARY KEY NOT NULL,
-    Intitule VARCHAR(100)
+    Intitule VARCHAR(100),
+    Groupe INT
 );
 
-INSERT INTO module VALUES ('MIN17214','Conception de BD');
-INSERT INTO module VALUES ('MIN17211','Methodes de Ranking');
-INSERT INTO module VALUES ('MIN17217','Application Web et Securite');
-INSERT INTO module VALUES ('MIN17213','Tuning de BD');
-INSERT INTO module VALUES ('MIN17215','Protocoles IP');
-INSERT INTO module VALUES ('MIN17218','Calcul Securise');
-INSERT INTO module VALUES ('MIN17212','Simulation');
-INSERT INTO module VALUES ('MIN17216','Reseaux etendus');
+-- Insertion des tuples --
+
+INSERT INTO module(Code, Intitule) VALUES ('MIN17214','Conception de BD');
+INSERT INTO module(Code, Intitule) VALUES ('MIN17211','Methodes de Ranking');
+INSERT INTO module(Code, Intitule) VALUES ('MIN17217','Application Web et Securite');
+INSERT INTO module(Code, Intitule) VALUES ('MIN17213','Tuning de BD');
+INSERT INTO module(Code, Intitule) VALUES ('MIN17215','Protocoles IP');
+INSERT INTO module(Code, Intitule) VALUES ('MIN17218','Calcul Securise');
+INSERT INTO module(Code, Intitule) VALUES ('MIN17212','Simulation');
+INSERT INTO module(Code, Intitule) VALUES ('MIN17216','Reseaux etendus');
