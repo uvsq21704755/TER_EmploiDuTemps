1222e71385e5c2efe6b1f15ff9640c341dc55715
Julie Varenne-Paquet <72136120+julieVP@users.noreply.github.com>
2022-04-16 12:56:14 +0200

    Update module.sql
    Modification des insert

--- SQL/module.sql
+++ SQL/module.sql
@@ -2,14 +2,13 @@ CREATE TABLE Module
 (
     Code VARCHAR(10) PRIMARY KEY NOT NULL,
     Intitule VARCHAR(100)
-)
+);
 
-INSERT INTO module VALUES 
-('MIN17214','Conception de BD'),
-('MIN17211','Methodes de Ranking'),
-('MIN17217','Application Web et Securite'),
-('MIN17213','Tuning de BD'),
-('MIN17215','Protocoles IP'),
-('MIN17218','Calcul Securise'),
-('MIN17212','Simulation'),
-('MIN17216','Reseaux etendus');
+INSERT INTO module VALUES ('MIN17214','Conception de BD');
+INSERT INTO module VALUES ('MIN17211','Methodes de Ranking');
+INSERT INTO module VALUES ('MIN17217','Application Web et Securite');
+INSERT INTO module VALUES ('MIN17213','Tuning de BD');
+INSERT INTO module VALUES ('MIN17215','Protocoles IP');
+INSERT INTO module VALUES ('MIN17218','Calcul Securise');
+INSERT INTO module VALUES ('MIN17212','Simulation');
+INSERT INTO module VALUES ('MIN17216','Reseaux etendus');
